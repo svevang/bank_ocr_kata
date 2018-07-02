83fb1a74b957d1a73969eb47c77a4c5e2574b2d5
Sam Vevang <sam.vevang@gmail.com>
2018-07-02 15:16:41 -0500

    red test for account number

--- lib/bank_ocr_kata.rb
+++ lib/bank_ocr_kata.rb
@@ -7,5 +7,12 @@ module BankOcrKata
     def self.read(path)
       contents = File.read(path)
     end
+
+    # Takes in an individual account number of 9 digits in the ascii art
+    # representation. There are 3 columns and 3 rows for each digit.
+    def self.account_number(digit_string)
+      digit_string.split("\n")
+    end
+
   end
 end
--- spec/digits_spec.rb
+++ spec/digits_spec.rb
@@ -1,7 +1,10 @@
 require "pry"
 
 RSpec.describe BankOcrKata::Digits do
-  let(:digits) do
+
+
+  # an unprocessed file for accounts
+  let(:digit_file_contents) do
   "    _  _     _  _  _  _  _ \n" +
   "  | _| _||_||_ |_   ||_||_|\n" +
   "  ||_  _|  | _||_|  ||_| _|\n" +
@@ -14,16 +17,40 @@ RSpec.describe BankOcrKata::Digits do
   "| || || || || || || || || |\n" +
   "|_||_||_||_||_||_||_||_||_|\n" + 
   "                           "
+  end
 
+  # digits here are split on the blank line
+  let (:digits_zeros) do
+  " _  _  _  _  _  _  _  _  _ \n" +
+  "| || || || || || || || || |\n" +
+  "|_||_||_||_||_||_||_||_||_|"
+  end
+
+  let (:digits_through_ten) do
+  "    _  _     _  _  _  _  _ \n" +
+  "  | _| _||_||_ |_   ||_||_|\n" +
+  "  ||_  _|  | _||_|  ||_| _|"
   end
 
   context "#read" do
     before do
-      allow(File).to receive(:read).with("digits.txt").and_return(digits)
+      skip
+      allow(File).to receive(:read).with("digit_file_contents.txt").and_return(digits)
     end
 
     subject(:parsed_digits) { BankOcrKata::Digits.read("digits.txt") }
 
     it { expect(parsed_digits.class).to be Array}
+  end
+
+  context ".account_number" do
+
+
+    subject(:parsed_account) { BankOcrKata::Digits.account_number(digits_through_ten) }
+
+    it { expect(parsed_account.class).to be Array }
+
+    it { expect(parsed_account).to all( be_an(Integer) ) }
+
   end
 end
